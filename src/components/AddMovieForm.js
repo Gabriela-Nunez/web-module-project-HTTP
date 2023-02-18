2aaac2eae18bc69bd3ba4f3b510cb679c207c9ac
Gabby Nunez <gabbynunez@Gabbys-MacBook-Pro.local>
2023-02-17 19:22:49 -0600

    AddMovieForm is complete and MVP was met.

--- src/components/AddMovieForm.js
+++ src/components/AddMovieForm.js
@@ -4,9 +4,9 @@ import { useParams, useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 
-const EditMovieForm = (props) => {
+const AddMovieForm = (props) => {
 	const { push } = useHistory();
-	const { id } = useParams();
+	
 
 	const { setMovies } = props;
 	const [movie, setMovie] = useState({
@@ -17,15 +17,6 @@ const EditMovieForm = (props) => {
 		description: ""
 	});
 
-	useEffect(() => {
-		axios.get(`http://localhost:9000/api/movies/${id}`)
-		.then(res => {
-			setMovie(res.data);
-		})
-		.catch(err => {
-			console.log(err.error);
-		})
-})
 	
 	const handleChange = (e) => {
         setMovie({
@@ -36,14 +27,15 @@ const EditMovieForm = (props) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        axios.put(`http://localhost:9000/api/movies/${id}`, movie)
-            .then(res=>{
-                setMovies(res.data);
-                push(`/movies/${movie.id}`);
-			})
-			.catch(err=>{
-				console.log(err);
-			})
+        axios.post(`http://localhost:9000/api/movies`, movie)
+        .then(res => {
+          props.setMovies(res.data)
+          push(`/movies`)
+        })
+        .catch(err => {
+          console.log(err.error)
+        })
+     
 	}
 	
 	const { title, director, genre, metascore, description } = movie;
@@ -53,7 +45,7 @@ const EditMovieForm = (props) => {
 		<div className="modal-content">
 			<form onSubmit={handleSubmit}>
 				<div className="modal-header">						
-					<h4 className="modal-title">Editing <strong>{movie.title}</strong></h4>
+					<h4 className="modal-title">Adding <strong>{movie.title}</strong></h4>
 				</div>
 				<div className="modal-body">					
 					<div className="form-group">
@@ -87,4 +79,4 @@ const EditMovieForm = (props) => {
 	</div>);
 }
 
-export default EditMovieForm;
+export default AddMovieForm;
